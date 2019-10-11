6ad07e21bb61163e41f58e0a21c101c1a3610bb0
潘慧娟 <pan_huij@163.com>
2019-10-11 15:07:24 +0800

    初始化脚手架

---
+++ src/pages/homepage/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="chart-container">
+    首页
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+</style>
+
---
+++ src/pages/survey/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="chart-container">
+    概况
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+</style>
+
