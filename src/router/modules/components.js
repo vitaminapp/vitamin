371b5732b7b4642cc038cf49a8c1c04c84542019
luxuan <1131576937@qq.com>
2019-10-13 20:27:00 +0800

    luxuan 1.3

--- src/router/modules/components.js
+++ src/router/modules/components.js
@@ -3,99 +3,105 @@
 import Layout from '@/layout'
 
 const componentsRouter = {
-  path: '/components',
+  path: '/order',
   component: Layout,
-  redirect: 'noRedirect',
-  name: 'ComponentDemo',
+  redirect: 'order',
+  name: 'order',
   meta: {
     title: '订单',
     icon: 'component'
   },
-  children: [
-    {
-      path: 'tinymce',
-      component: () => import('@/views/components-demo/tinymce'),
-      name: 'TinymceDemo',
-      meta: { title: 'Tinymce' }
-    },
-    {
-      path: 'markdown',
-      component: () => import('@/views/components-demo/markdown'),
-      name: 'MarkdownDemo',
-      meta: { title: 'Markdown' }
-    },
-    {
-      path: 'json-editor',
-      component: () => import('@/views/components-demo/json-editor'),
-      name: 'JsonEditorDemo',
-      meta: { title: 'JSON Editor' }
-    },
-    {
-      path: 'split-pane',
-      component: () => import('@/views/components-demo/split-pane'),
-      name: 'SplitpaneDemo',
-      meta: { title: 'SplitPane' }
-    },
-    {
-      path: 'avatar-upload',
-      component: () => import('@/views/components-demo/avatar-upload'),
-      name: 'AvatarUploadDemo',
-      meta: { title: 'Upload' }
-    },
-    {
-      path: 'dropzone',
-      component: () => import('@/views/components-demo/dropzone'),
-      name: 'DropzoneDemo',
-      meta: { title: 'Dropzone' }
-    },
-    {
-      path: 'sticky',
-      component: () => import('@/views/components-demo/sticky'),
-      name: 'StickyDemo',
-      meta: { title: 'Sticky' }
-    },
-    {
-      path: 'count-to',
-      component: () => import('@/views/components-demo/count-to'),
-      name: 'CountToDemo',
-      meta: { title: 'Count To' }
-    },
-    {
-      path: 'mixin',
-      component: () => import('@/views/components-demo/mixin'),
-      name: 'ComponentMixinDemo',
-      meta: { title: 'Component Mixin' }
-    },
-    {
-      path: 'back-to-top',
-      component: () => import('@/views/components-demo/back-to-top'),
-      name: 'BackToTopDemo',
-      meta: { title: 'Back To Top' }
-    },
-    {
-      path: 'drag-dialog',
-      component: () => import('@/views/components-demo/drag-dialog'),
-      name: 'DragDialogDemo',
-      meta: { title: 'Drag Dialog' }
-    },
-    {
-      path: 'drag-select',
-      component: () => import('@/views/components-demo/drag-select'),
-      name: 'DragSelectDemo',
-      meta: { title: 'Drag Select' }
-    },
-    {
-      path: 'dnd-list',
-      component: () => import('@/views/components-demo/dnd-list'),
-      name: 'DndListDemo',
-      meta: { title: 'Dnd List' }
-    },
-    {
-      path: 'drag-kanban',
-      component: () => import('@/views/components-demo/drag-kanban'),
-      name: 'DragKanbanDemo',
-      meta: { title: 'Drag Kanban' }
-    }
+  children: [{
+    path: 'storeManagement',
+    component: () =>
+                import('@/pages/order/storeManagement'),
+    name: 'storeManagement',
+    meta: { title: '订单管理' }
+  },
+  {
+    path: 'singleback',
+    component: () =>
+                import('@/pages/order/singleback'),
+    name: 'singleback',
+    meta: { title: '整单退' }
+  },
+  {
+    path: 'shopOrder',
+    component: () =>
+                import('@/pages/order/shopOrder'),
+    name: 'shopOrder',
+    meta: { title: '店铺订单' }
+  },
+  {
+    path: 'refundManagement',
+    component: () =>
+                import('@/pages/order/refundManagement'),
+    name: 'refundManagement',
+    meta: { title: '退款管理' }
+  },
+  {
+    path: 'refundAudit',
+    component: () =>
+                import('@/pages/order/refundAudit'),
+    name: 'refundAudit',
+    meta: { title: '退款审核' }
+  },
+  {
+    path: 'orderManagement',
+    component: () =>
+                import('@/pages/order/orderManagement'),
+    name: 'orderManagement',
+    meta: { title: '订单管理' }
+  },
+  {
+    path: 'invoicemanagement',
+    component: () =>
+                import('@/pages/order/invoicemanagement'),
+    name: 'invoicemanagement',
+    meta: { title: '发票管理' }
+  },
+  {
+    path: 'financialRefund',
+    component: () =>
+                import('@/pages/order/financialRefund'),
+    name: 'financialRefund',
+    meta: { title: '财务退款' }
+  },
+  {
+    path: 'back-to-top',
+    component: () =>
+                import('@/views/components-demo/back-to-top'),
+    name: 'BackToTopDemo',
+    meta: { title: 'Back To Top' }
+  },
+  {
+    path: 'drag-dialog',
+    component: () =>
+                import('@/views/components-demo/drag-dialog'),
+    name: 'DragDialogDemo',
+    meta: { title: 'Drag Dialog' }
+  },
+  {
+    path: 'drag-select',
+    component: () =>
+                import('@/views/components-demo/drag-select'),
+    name: 'DragSelectDemo',
+    meta: { title: 'Drag Select' }
+  },
+  {
+    path: 'dnd-list',
+    component: () =>
+                import('@/views/components-demo/dnd-list'),
+    name: 'DndListDemo',
+    meta: { title: 'Dnd List' }
+  },
+  {
+    path: 'drag-kanban',
+    component: () =>
+                import('@/views/components-demo/drag-kanban'),
+    name: 'DragKanbanDemo',
+    meta: { title: 'Drag Kanban' }
+  }
   ]
 }
 
